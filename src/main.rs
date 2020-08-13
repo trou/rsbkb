d611e8806220b387cc2af4968614016dd2d03f41
Raphaël Rigo <devel-git@syscall.eu>
2020-08-13 23:45:27 +0200

    rollback: consider args as Str instead of OsStr

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,14 @@
 //trace_macros!(true); 
 use std::env;
 use std::io;
+use std::path::Path;
 use std::io::{Read, Write};
-//use std::fs::{OpenOptions};
 extern crate hex;
 extern crate base64;
 extern crate percent_encoding;
 extern crate clap;
 extern crate crc;
 use atty::Stream;
-
-// Allow the use of as_bytes() on OsStr
-use std::os::unix::ffi::OsStrExt;
 
 mod applet;
 use applet::Applet;
@@ -58,9 +55,13 @@ fn main() {
     let app_names : Vec<_> = apps.iter().map(|app| app.command()).collect();
 
     /* Get arg0 */
-    let mut args: Vec<_>= env::args_os().collect();
-    let arg0 = env::current_exe().expect("Can't get executable name :");
-    let arg0 = arg0.file_name().expect("No filename for executable filename ?");
+    let mut args: Vec<_>= env::args().collect();
+
+    let arg0 = Path::new(&args[0]).file_name();
+    let arg0 = match arg0 {
+        Some(a) => a.to_str().unwrap().to_string(),
+        None => panic!("No arg0"),
+    };
 
     let mut app = clap::App::new("rsbkb")
         .version("0.3.0")
@@ -69,9 +70,8 @@ fn main() {
         .subcommands(apps.iter().map(|app| app.subcommand()));
 
     // Check if arg0 is a supported subcommand and use it
-    let arg0_str = &arg0.to_str().unwrap_or("");
-    if app_names.contains(arg0_str) {
-        args.insert(1, arg0.to_os_string());
+    if app_names.contains(&arg0.as_str()) {
+        args.insert(1, arg0);
     }
 
     // Parse args
@@ -93,7 +93,7 @@ fn main() {
         if !sub_matches.is_present(argname) {
             io::stdin().read_to_end(&mut inputval).expect("Reading stdin failed");
         } else {
-            inputval = sub_matches.value_of_os(argname).unwrap().as_bytes().to_vec();
+            inputval = sub_matches.value_of(argname).unwrap().as_bytes().to_vec();
         }
     };
 
