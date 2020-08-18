dc5f35c97459df7bdd3f7a80fd79b95e8253a2a3
Raphaël Rigo <devel@syscall.eu>
2020-08-18 18:53:36 +0200

    WIP ts

--- src/timeapp.rs
+++ src/timeapp.rs
@@ -16,8 +16,57 @@ impl Applet for TimeApplet {
     }
 
     fn process(&self, val: Vec<u8>) -> Vec<u8> {
+        /*
+
+            # Windows FileTime (18 digits)
+            if 130645440000000000 <= timestamp <= 133801632000000000:  # 2015 <= ts <= 2025
+                new_timestamp = decode_windows_filetime(timestamp)
+
+            # .Net/C# DateTime ticks (18 digits)
+            elif 635556672000000000 <= timestamp <= 638712864000000000:  # 2015 <= ts <= 2025
+                new_timestamp = decode_datetime_ticks(timestamp)
+
+            # WebKit (17 digits)
+            elif 13064544000000000 <= timestamp <= 13380163200000000:  # 2015 <= ts <= 2025
+                new_timestamp = decode_webkit(timestamp)
+
+            # Epoch microseconds (16 digits)
+            elif 1420070400000000 <= timestamp <= 1735689600000000:  # 2015 <= ts <= 2025
+                new_timestamp = decode_epoch_microseconds(timestamp)
+
+            # Epoch milliseconds (13 digits)
+            elif 1420070400000 <= timestamp <= 1735689600000:  # 2015 <= ts <= 2025
+                new_timestamp = decode_epoch_milliseconds(timestamp)
+
+            # Epoch seconds (10 digits)
+            elif 1420070400 <= timestamp <= 1735689600:  # 2015 <= ts <= 2025
+                new_timestamp = decode_epoch_seconds(timestamp)
+
+            # Mac Absolute Time (9 digits)
+            elif 441763200 <= timestamp <= 757382400:  # 2015 <= ts <= 2025
+                new_timestamp = decode_mac_absolute_time(timestamp)
+
+        elif matches_float:
+            timestamp = float(node.value)
+
+            # Epoch seconds (10 digits)
+            if 1420070400.0 <= timestamp <= 1735689600.0:  # 2015 <= ts <= 2025
+                new_timestamp = decode_epoch_seconds(timestamp)
+
+            # Mac Absolute Time (9 digits)
+            elif 441763200.0 <= timestamp <= 757382400.0:  # 2015 <= ts <= 2025
+                new_timestamp = decode_mac_absolute_time(timestamp)
+                */
         let ts_str = String::from_utf8(val).unwrap();
-        let ts = OffsetDateTime::from_unix_timestamp(i64::from_str_with_radix(ts_str.as_str()).unwrap());
+        let ts_len = ts_str.len();
+        let ts_int = i64::from_str_with_radix(ts_str.as_str()).unwrap();
+        let ts =
+            match (ts_len, ts_int) {
+                // Windows FILETIME
+                //(18, _) =>
+                (10, 1420070400..=1735689600) => OffsetDateTime::from_unix_timestamp(ts_int),
+                _ => OffsetDateTime::from_unix_timestamp(ts_int)
+            };
         let date_str = ts.format("%F %T");
         return date_str.as_bytes().to_vec();
     }
