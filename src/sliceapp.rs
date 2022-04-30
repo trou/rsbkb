c01deeb1bd5ceb6352370ccc5f542f9610b91ef7
Raphaël Rigo <devel-git@syscall.eu>
2022-04-30 20:33:13 +0200

    Implement negative offset for slice. Fix #11

--- src/sliceapp.rs
+++ src/sliceapp.rs
@@ -8,6 +8,7 @@ use crate::applet::{Applet, FromStrWithRadix};
 pub struct SliceApplet {
     file :  Option<String>,
     start : u64,
+    from_end : bool,
     end: Option<u64>
 }
 
@@ -18,20 +19,26 @@ impl Applet for SliceApplet {
     fn subcommand(&self) -> App {
         SubCommand::with_name(self.command()).about(self.description())
                 .arg_from_usage("<file>   'file to slice'")
-                .arg_from_usage("<start>  'start of slice'")
+                .arg_from_usage("<start>  'start of slice, relative to end of file if negative'")
                 .arg_from_usage("[end]  'end of slice: absolute or relative if prefixed with +'")
     }
 
     fn arg_or_stdin(&self) -> Option<&'static str> { None }
 
     fn new() ->  Box<dyn Applet> {
-        Box::new(Self { file: None, start: 0, end: None})
+        Box::new(Self { file: None, start: 0, from_end: false, end: None})
     }
 
     fn parse_args(&self, args: &clap::ArgMatches) -> Box<dyn Applet> {
         let filename = args.value_of("file").unwrap();
-        let start_va = args.value_of("start").unwrap();
-        let start = u64::from_str_with_radix(start_va).expect("invalid start");
+        let start_val = args.value_of("start").unwrap();
+
+        /* Negative start: offset from the end. */
+        let (start, from_end) = if let Some(start_val_no_plus) = start_val.strip_prefix("-") {
+                (u64::from_str_with_radix(start_val_no_plus).expect("Invalid start"), true)
+        } else {
+                (u64::from_str_with_radix(start_val).expect("Invalid start"), false)
+        };
 
         let end: Option<u64> = if let Some(end_val) = args.value_of("end") {
             if let Some(end_val_no_plus) = end_val.strip_prefix("+") {
@@ -41,7 +48,7 @@ impl Applet for SliceApplet {
             }
         } else { None };
 
-        Box::new(Self {file: Some(filename.to_string()), start, end })
+        Box::new(Self {file: Some(filename.to_string()), start, from_end, end })
     }
 
     fn process(&self, _val: Vec<u8>) -> Vec<u8> {
@@ -50,7 +57,11 @@ impl Applet for SliceApplet {
         let mut f =
         BufReader::new(OpenOptions::new().read(true).write(false).open(filename).expect("can't open file"));
 
-        f.seek(SeekFrom::Start(self.start)).expect("Seek failed");
+        if self.from_end {
+            f.seek(SeekFrom::End(-(self.start as i64))).expect("Seek failed");
+        } else {
+            f.seek(SeekFrom::Start(self.start)).expect("Seek failed");
+        }
         let mut res =  vec![];
         if self.end.is_some() {
             let end = self.end.unwrap();
