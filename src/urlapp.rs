95421cf2103303c4b52e49737882c7c71c604a63
Raphaël Rigo <devel-git@syscall.eu>
2025-03-19 07:46:57 +0100

    urlenc: test encoding of 00 and FF

--- src/urlapp.rs
+++ src/urlapp.rs
@@ -208,6 +208,17 @@ mod tests {
     }
 
     #[test]
+    fn test_urlenc_00_ff() {
+        let mut table = [false; 256];
+        build_default_table(&"".to_string(), &mut table);
+        let urlenc = UrlEncApplet { table: table };
+        let encoded = urlenc
+            .process([0, 0xFF].to_vec())
+            .expect("encoding failed");
+        assert_eq!(String::from_utf8(encoded).unwrap(), "%00%ff");
+    }
+
+    #[test]
     fn test_urlencdec() {
         let mut table = [false; 256];
         build_default_table(&"".to_string(), &mut table);
